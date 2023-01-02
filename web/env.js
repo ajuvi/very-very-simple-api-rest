ff271e56a11cedcfbd3353ccc7095cf6ba23cd5a
ajuvi <ajuve@institutmontilivi.cat>
2023-01-03 00:47:14 +0100

    artur

--- web/env.js
+++ web/env.js
@@ -1,6 +1,6 @@
-var env_rest = "http://localhost/very-very-simple-api-rest/api/";
+var api_rest = "http://localhost/very-very-simple-api-rest/api";
 
 function url_rest(resource){
-    return env_rest + resource;
+    return api_rest + resource;
 }
 
